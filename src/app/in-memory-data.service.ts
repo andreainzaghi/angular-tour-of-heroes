19b27f70aa8bcbd2212e9487712756af8bf4390c
andreainzaghi <andreainzaghilavoro@gmail.com>
2022-06-13 13:01:35 +0200

    angular-tour

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 1, name: 'Dr. Nice' },
+      { id: 2, name: 'Dr. Nice' },
+      { id: 3, name: 'Bombasto' },
+      { id: 4, name: 'Celeritas' },
+      { id: 5, name: 'Magneta' },
+      { id: 6, name: 'RubberMan' },
+      { id: 7, name: 'Dynama' },
+      { id: 8, name: 'Dr. IQ' },
+      { id: 9, name: 'Magma' },
+      { id: 10, name: 'Tornado' },
+      { id: 11, name: 'Tornado' },
+      { id: 12, name: 'Dr. Nice' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr. IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
+    ];
+    return { heroes };
+  }
+
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+}
